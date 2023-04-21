a195d4c993b7e5fd1c19e31f733b0da1d197230b
ProPanek <oga.seb@gmail.com>
2023-04-21 13:10:57 +0200

    fix fs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import fs from "fs";
+import * as fs from "fs";
 
 async function bootstrap() {
   const httpsOptions = {
