ac9d4867542b61a269cba9ba149f742520bc94d9
ProPanek <oga.seb@gmail.com>
2023-04-21 13:09:22 +0200

    adding ssl to nestjs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import fs from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync("./secrets/privkey.pem"),
+    cert: fs.readFileSync("./secrets/fullchain.pem"),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.setGlobalPrefix("api");
   const corsOptions = {
     origin: "*",
